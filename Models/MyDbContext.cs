829d998458653b23d9321a6f892192b38d563b57
ACE <golden-ace@gmail.com>
2024-12-02 08:23:58 +0900

    complete

---
+++ Models/MyDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eshop.Models
+{
+    public class MyDbContext : DbContext
+    {
+
+        public DbSet<Person> People {  get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("MyDb");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
